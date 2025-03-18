ff9abd47477421d4f394ef8465fd0623150f35c7
dapaz-programador <hermesdapaz.programador@gmail.com>
2025-03-18 00:21:36 -0300

    primeira parte exercicio

---
+++ exercicios/desafio01/exercicio01.js
@@ -0,0 +1,43 @@
+// Por que fazer esses exercicios ? Agora é a hora de treinar seu cérebro a como "conversar com o computador".
+// Você precisa desenvolver um lógica forte, para enfrentar qualquer desafio.
+// Desenvolver racio
+
+// exercicio 01 - Faça um programa que SOME 2 números.
+
+let numero1 = 25;
+let numero2 = 30;
+
+let soma = numero1 + numero2;
+
+console.log(soma); // resultado = 55
+
+//exercicio 02 - Faça um programa que mutiplique 2 números e o resultado adicione 10.
+
+let mutiplicacao = numero1 * numero2;
+let resultado = mutiplicacao + 10;
+
+console.log(resultado);  // resultado = 760
+
+
+
+// exercicio 03 - Faça um programa que encontre a raiz quadrada de um número, multiplique o resultado por 10 e divida por 33.
+
+let raiz = Math.sqrt(numero1,2);
+let mutiplicacao2 = 10 * raiz;
+const divisao1 = mutiplicacao2 / 33;
+
+console.log(divisao1); //resultado = 1.515151515151
+
+
+
+// exercicio 04 - Faça um programa que inicie com dois nomes , e o programa imprima na tela o seguinte dado:
+// Olá, meu nome é (nome1) e meu partner do CodeClube é (nome2)
+
+let nome1 = "josé";
+let nome2 = "joão";
+
+console.log(`Olá, meu nome é ${nome1} e meu partner do CodeClube é ${nome2}`);  // resultado = Olá, meu nome é josé e meu partner do CodeClube é joão
+
+
+
+// exercicio 05 - Faça um programa que 
---
+++ firstproject.js
@@ -0,0 +1,10 @@
+var number1 = 1; //não usar o var
+let number2 = 2;
+const number3 = 3;
+
+number1 = 10
+number2 = 20
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
